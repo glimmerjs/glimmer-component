d04c0d9f7b2d6c306cfd69fda90be8f977ea3916
Robbie Pitts <me@robbie-pitts.com>
2017-06-21 16:45:16 -0400

    Expose `RootReference`

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as ComponentDefinition } from "./component-definition";
 export { default as ComponentManager } from "./component-manager";
 export { tracked, setPropertyDidChange } from "./tracked";
 export { default as TemplateMeta } from './template-meta';
+export { RootReference } from './references';
